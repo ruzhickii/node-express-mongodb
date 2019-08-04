1dc804149dd9dc9c2d9de62d75068b2358c5cd63
Alexander Ruzhickii <ruzhickiisasha@gmail.com>
2019-08-04 18:56:44 +0300

    routes registration

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 
+const homeRoutes = require('./routes/home');
+const addRoutes = require('./routes/add');
+const coursesRoutes = require('./routes/courses');
+
 const app = express();
 
 
@@ -17,31 +21,14 @@ app.set('views', 'views');
 
 
 app.use(express.static('public'));
+app.use('/', homeRoutes);
+app.use('/add', addRoutes);
+app.use('/courses', coursesRoutes);
 
 
-app.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Home',
-        isHome: true
-    });
-});
-
-app.get('/courses', (req, res) => {
-    res.render('courses', {
-        title: 'Courses',
-        isCourses: true
-    });
-});
-
-app.get('/add', (req, res) => {
-    res.render('add', {
-        title: 'Add course',
-        isAdd: true
-    });
-});
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(3000, () => {
-    console.log(`Server is runing on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
